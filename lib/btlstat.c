aef616ec6264af2c27f1d8ee86fd2b7e066ae8e1
Stuart Gathman <stuart@gathman.org>
1994-03-17 23:46:17 +0000

    use STAT name feature of kernel

--- lib/btlstat.c
+++ lib/btlstat.c
@@ -10,18 +10,14 @@ int btlstat(const BTCB *btcb,struct btstat *p,struct btlevel *l) {
   bt.flags = 0;
   bt.u.id.user = getuid();
   bt.u.id.group = getgid();
-  bt.u.id.mode = BT_DIR;
+  bt.u.id.mode = 0;
   catch(rc)
     memcpy(bt.lbuf,btcb->lbuf,bt.klen = btcb->klen);
     bt.rlen = sizeof bt.lbuf;
-    btas(&bt,BTOPEN+BT_DIR+LOCK);	/* open */
-    bt.klen = 0;
-    bt.rlen = sizeof *p;
     (void)btas(&bt,BTSTAT);		/* get scoop */
     memcpy((char *)p,bt.lbuf,sizeof *p);
     l->node = bt.root;
     l->slot = bt.mid;
   envend
-  (void)btas(&bt,BTCLOSE);		/* close */
   return rc;
 }
