1a04248ad0a49de1d89804cb8969fa559dd8b3bd
Stuart Gathman <stuart@gathman.org>
1994-03-28 20:17:56 +0000

    make {no}hash.c interface clearer

--- btbuf.h
+++ btbuf.h
@@ -1,26 +1,32 @@
 #include "btree.h"
 
 #define SECT_SIZE	512	/* OS sector size (min raw I/O size) */
+#define MAXFLUSH	60	/* maximum modified blocks allowed */
 
-extern int btbegin(/**/ int ,unsigned /**/);	/* initialize */
-extern short btmount(/**/ char * /**/);		/* mount fs */
-extern int btroot(/**/ t_block, int /**/);	/* declare current root */
-extern void btget(/**/ int /**/);		/* reserve buffers */
-extern BLOCK *btbuf(/**/ t_block /**/);		/* read block */
-extern BLOCK *btread(/**/ t_block /**/);	/* read block no root check */
-extern BLOCK *btnew(/**/ int /**/);		/* allocate block */
-extern BLOCK *getbuf(/**/ t_block, int /**/);	/* find block, don't read */
+/* btbuf.c */
+int btbegin(int ,unsigned);	/* initialize */
+short btmount(const char *);	/* mount fs */
+int btroot(t_block, short);	/* declare current root */
+BLOCK *btbuf(t_block);		/* read block */
+BLOCK *btread(t_block);	/* read block no root check */
+BLOCK *btnew(short);		/* allocate block */
 extern int newcnt;				/* counts new/deleted blocks */
 extern int safe_eof;				/* safe btspace() */
 extern long curtime;				/* BTAS time */
-extern void btfree(/**/ BLOCK * /**/);		/* free block */
-extern void swapbuf(/**/ BLOCK *, BLOCK * /**/);/* swap block ids */
-extern int btumount(/**/ int /**/);		/* unmount fs */
-extern int btflush(/**/ void /**/);		/* flush all buffers */
-extern void btend(/**/ void /**/);		/* shutdown */
+void btfree(BLOCK *);		/* free block */
+int btumount(short);		/* unmount fs */
+int btflush(void);		/* flush all buffers */
+void btend(void);		/* shutdown */
 
 extern struct btfhdr devtbl[MAXDEV];		/* mid table */
-extern int gethdr(/**/ const struct btfhdr *, char *, int /**/);
-extern void btspace(/**/ void /**/);		/* check free space */
+int gethdr(const struct btfhdr *, char *, int);
+void btspace(void);		/* check free space */
 
 extern struct btpstat stat;
+
+/* hash.c || nohash.c */
+BLOCK *getbuf(t_block, short);	/* find block, don't read */
+void swapbuf(BLOCK *, BLOCK *);/* swap block ids */
+void btget(int);		/* reserve buffers */
+void btget_deb(int,const char *,int);
+#define btget(i) btget_deb(i,what,__LINE__)
