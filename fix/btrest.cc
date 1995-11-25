4f110995d4a2c555bd63571b722d4895943e44e9
Stuart Gathman <stuart@gathman.org>
1995-11-25 02:32:57 +0000

    support converting to checkpoint format

--- fix/btrest.cc
+++ fix/btrest.cc
@@ -3,29 +3,43 @@
 	Copyright 1991 Business Management Systems, Inc.
 */
 // $Log$
+// Revision 1.3  1995/11/08  19:36:46  stuart
+// oops
+//
 // Revision 1.2  1995/11/02  15:33:59  stuart
 // rearrange RCS id
 //
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
 #include <io.h>
 #include "fs.h"
 
 // FIXME: can only restore one extent
 
+enum { SECT_SIZE = 512 };
+
 int main(int argc,char **argv) {
-  bool convert68k = false;
+  bool convert68k = false;	// convert from 68k format
+  unsigned chk = 0;		// checkpoint size
   int i;
   for (i = 1; i < argc; ++i) {
     if (strcmp(argv[i],"-6") == 0)
       convert68k = true;
+    else if (strncmp(argv[i],"-c",2) == 0) {
+      if (argv[i][2])
+	chk = atoi(argv[i] + 2);
+      else
+	chk = atoi(argv[++i]);
+    }
     else
       break;
   }
   if (i + 1 != argc) {
     fputs("$Id$\n\
-Usage:	btrest [-6] osfile <archive\n\
+Usage:	btrest [-6] [-cchksize] osfile <archive\n\
 	-6	convert root nodes from 68k to RISC format (add padding)\n\
+	-c	specify checkpoint size to reserve\n\
 	osfile	filesystem to overwrite, - for stdout\n",
 	stderr);
     return 1;
@@ -73,13 +87,48 @@ Usage:	btrest [-6] osfile <archive\n\
   {
     union {
       btfs f;
-      char buf[512];
+      char buf[SECT_SIZE];
     };
-    f = *fs.hdr();
+    bool seekable = false;
+    if (chk) {
+      long superoffset = SECT_SIZE;
+      seekable = true;
+      if (fseek(outf,superoffset,0)) {
+	memset(buf,0,sizeof buf);
+	if (fwrite(buf,sizeof buf,1,outf) != 1) {
+	  perror(outputname);
+	  return 1;
+	}
+      }
+      f = *fs.hdr();
+      f.hdr.root = (superoffset + SECT_SIZE) / SECT_SIZE + chk;
+      int blksects = f.hdr.blksize / SECT_SIZE;
+      if (f.hdr.root % blksects)
+	f.hdr.root += blksects - f.hdr.root % blksects;
+    }
+    else
+      f = *fs.hdr();
+    long rootpos = f.hdr.root * SECT_SIZE;
     f.hdr.magic = BTMAGIC;
     if (fwrite(buf,sizeof buf,1,outf) != 1) {
       perror(outputname);
       return 1;
+    }
+    if (seekable) {
+      if (fseek(outf,rootpos,0)) {
+	perror(outputname);
+	return 1;
+      }
+    }
+    else {
+      memset(buf,0,sizeof buf);
+      while (rootpos > SECT_SIZE) {
+	if (fwrite(buf,sizeof buf,1,outf) != 1) {
+	  perror(outputname);
+	  return 1;
+	}
+	rootpos -= SECT_SIZE;
+      }
     }
   }
   do {
