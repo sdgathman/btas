1d568b0167014fe8e7927505fae1e7864bb37bb3
Stuart Gathman <stuart@gathman.org>
1996-12-17 16:53:44 +0000

    btree manipulation

--- btree.h
+++ btree.h
@@ -1,21 +1,18 @@
+#ifndef __BTREE_H
+#define __BTREE_H
+#include <port.h>
 #include <stdlib.h>
 #include <setjmp.h>
 
-#define STATIC		/* private functions made extern for debugging */
-
-#define MAXLEV  8		/* maximum tree levels */
-#define MAXBUF  (MAXLEV + 2)	/* maximum buffers reserved */
+enum {
+  MAXLEV = 8,		/* maximum tree levels */
+  MAXBUF = MAXLEV + 2,	/* maximum buffers reserved */
+  SECT_SIZE = 512
+};
 
 #include "bttype.h"
 
-#define blk_dev(b)	((unsigned char)((b) >> 24))	/* device index */
-#define blk_off(b)	((b) & 0xFFFFFFL)		/* block offset */
-#define mk_blk(i,b)	(((long)(i)<<24)|(b))
-
-/* byte position of block */
-#define blk_pos(b,s)	(((b)-1) * (long)(s) + (long)SECT_SIZE)
-
-extern struct btlevel stack[];
+extern struct btlevel stack[MAXLEV];
 extern struct btlevel *sp;
 
 /* make sure son is same offset for stem & root
@@ -45,32 +42,11 @@ union btree {
     char data[BLKSIZE];	/* but write to disk as char array */
 };
 
-typedef struct block {
-  struct block *lru;
-#ifndef __MSDOS__
-  struct block *mru;	/* lru buffer chain */
-#endif
-  t_block blk;		/* block address */
-  short mid;		/* mount id */
-  short flags;
-  short count;		/* holds count while in memory */
-  union node *np;	/* pointer to data area of node */
-  union btree buf;
-} BLOCK;
-
-/* flag values */
-#define BLK_MOD		001	/* block modified */
-#define BLK_LOW		002	/* block is low priority */
-#define BLK_CHK		004	/* block has been checkpointed */
-#define BLK_ROOT	010	/* root node */
-#define BLK_KEY		020	/* block id has been modified */
-#define BLK_DIR		040	/* block belongs to a directory */
-#define BLK_TOUCH      0100	/* block is in touched list */
-#define BLK_STEM	(unsigned short)0x8000	/* key data */
-
 struct BTCB;
-BLOCK *bttrace(struct BTCB *, int, int);
+struct BLOCK *bttrace(struct BTCB *, int, int);
 void btadd(char *, int);
 void btdel(void);
 extern jmp_buf btjmp;
+extern int maxrec;	/* maximum record size */
 #define btpost(c)	longjmp(btjmp,c)
+#endif
