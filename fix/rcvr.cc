1a73a3432160217e13c77ca82c75038d32026274
Stuart Gathman <stuart@gathman.org>
1999-06-21 18:58:43 +0000

    hooks for applications that rebuild directory structure.

--- fix/rcvr.cc
+++ fix/rcvr.cc
@@ -1,9 +1,13 @@
 /* $Log$
+ * Revision 1.1  1999/05/10  22:40:47  stuart
+ * Initial revision
+ *
  */
 #pragma implementation
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 #include <errenv.h>
 #include <bterr.h>
 #include <ftype.h>
@@ -11,8 +15,6 @@
 #include "../node.h"
 #include "rcvr.h"
 #include <new.h>
-
-static t_block dirstat(BTCB *, const char *, struct btstat *);
 
 rcvr::rcvr() {
   tag.flags = 0;
@@ -23,16 +25,16 @@ rcvr::rcvr() {
   dir  = 0;
   name = 0;
   undo = false;
-}
-
-void rcvr::setName(const char *n,long rt) {
-  if (!name) {
-    new(this) rcvr(n,rt,false,false);
-  }
+  memset(&st,0,sizeof st);
 }
 
 rcvr::rcvr(const char *n,long rt,bool test,bool interactive) {
   new(this) rcvr;
+  setName(n,rt,test,interactive);
+}
+
+void rcvr::setName(const char *n,long rt,bool test,bool interactive) {
+  if (name) return;
   name = new char[strlen(n) + 1];
   if (!name) return;
   strcpy(name,n);
@@ -47,15 +49,16 @@ rcvr::rcvr(const char *n,long rt,bool test,bool interactive) {
     dirf = btopendir(n,mode);
     /* check filename and get root id if required */
     if (dirf->flags & BT_DIR) {
-      struct btstat st;
       dirf->rlen = MAXREC;
-      root = dirstat(dirf,p,&st);
+      root = dirstat(dirf,p,interactive);
       if (root == -1L) {
 	printf("%s: different filesystem.\n",p);
 	errpost(BTERMID);
       }
+      if (dir)
+        st.id.mode |= (BT_DIR | 0111);
       int rc = btas(dirf,BTREADEQ + NOKEY);
-      if (rc == 0) {
+      if (rc == 0 || p[0] == 0) {
 	if (interactive)
 	  puts("Records will be added to the existing file.");
 	if (!rt || !root) {
@@ -74,8 +77,8 @@ rcvr::rcvr(const char *n,long rt,bool test,bool interactive) {
       else
 	dirf->rlen = dirf->klen;	/* no field table */
       if (root == 0) {
-	printf("%s: not found.\n",p);
 	if (interactive) {
+	  printf("%s: not found.\n",p);
 	  puts("NOTE: the recovered file will have no field table,");
 	  puts("      but the records will be correct.");
 	}
@@ -85,15 +88,20 @@ rcvr::rcvr(const char *n,long rt,bool test,bool interactive) {
 	if (rc)
 	  printf("test: CREATE %s\n",dirf->lbuf);
       }
-      else {
-	dirf->u.id = st.id;
+      else if (*p) {
+	dirf->u.id = st.id;	
+	dirf->u.id.mode &= ~077;	/* initially accessible by us only */
 	(void)btas(dirf,BTCREATE + DUPKEY);	/* create new file */
       }
       BTCB b;
       btopenf(&b,name,mode + BTDIROK,sizeof b.lbuf);
       btcb2tag(&b,&tag);
-      printf("%s: root %08lX from %08lX\n",p,tag.root,root);
-      tag.flags &= ~BT_DIR;	/* treat directories as files */
+      if (interactive)
+	printf("%s: root %08lX from %08lX\n",p,tag.root,root);
+      // FIXME: recovering an individual directory should be restricted 
+      // to "in place".
+      if (!dir)
+	tag.flags &= ~BT_DIR;	/* treat directories as files */
     }
     else {
       printf("%s: invalid path\n",name);
@@ -106,8 +114,16 @@ rcvr::rcvr(const char *n,long rt,bool test,bool interactive) {
 
 rcvr::~rcvr() {
   if (tag.flags) {
-    BTCB *b = (BTCB *)alloca(btsize(0));
+    BTCB *b = (BTCB *)alloca(btsize(sizeof st));
     tag2btcb(b,&tag);
+    memcpy(b->lbuf,&st,sizeof st);
+    b->u.cache.node = 0L;
+    b->rlen = sizeof st;
+    b->klen = sizeof st;
+    b->u.id.user = geteuid();
+    b->u.id.group = getegid();
+    b->u.id.mode = 0666;
+    btas(b,BTTOUCH);
     b->klen = b->rlen = 0;
     btas(b,BTCLOSE);
   }
@@ -147,7 +163,7 @@ void rcvr::write(BTCB *b) {
     if (len <= 4) return;	// garbage directory record
     len -= 4;
     t_block root = ldlong(b->lbuf + len);
-    dir->link(b->lbuf,len,root);
+    dir->link(this,b->lbuf,len,root);
     return;
   }
   if (test()) {
@@ -157,6 +173,20 @@ void rcvr::write(BTCB *b) {
   }
   if (btas(b,BTWRITE + DUPKEY))
     ++dups;
+}
+
+void rcvr::touch(const struct btstat *stat) {
+  st = *stat;	// save file attributes to be applied in dtor
+}
+
+bool rcvr::link(const char *buf,int len,t_block root) {
+  BTCB *b = (BTCB *)alloca(btsize(len + 4));
+  tag2btcb(b,&tag);
+  memcpy(b->lbuf,buf,len);
+  b->u.cache.node = root;
+  b->rlen = len;
+  b->klen = len;
+  return btas(b,BTLINK + DUPKEY) == 0;
 }
 
 // write records in a node to recovered file.
@@ -174,7 +204,7 @@ int rcvr::donode(NODE *np,int maxrec,void *blkend) {
   tag2btcb(b,&tag);
   while (--i) {
     unsigned char *p = np->rptr(i);
-    b->rlen = np->size(i) + *p;
+    b->rlen = np->size(i) + *p - 1;
     if (b->rlen > maxrec) break;
     memcpy(b->lbuf + *p,p + 1,b->rlen - *p);
     b->klen = b->rlen;
@@ -192,7 +222,7 @@ void rcvr::summary() const {
   printf("%8ld %8ld %8ld %s\n",blks,recs,dups,name);
 }
 
-static t_block dirstat(BTCB *dirf,const char *name,struct btstat *st) {
+t_block rcvr::dirstat(BTCB *dirf,const char *name,bool interactive) {
   BTCB b;
   int rc;
   b.root = dirf->root;
@@ -205,23 +235,24 @@ static t_block dirstat(BTCB *dirf,const char *name,struct btstat *st) {
   (void)memcpy(b.lbuf,name,b.klen);
   catch(rc)
     btas(&b,BTOPEN);
-    b.rlen = sizeof *st;
+    b.rlen = sizeof st;
     b.klen = 0;
     btas(&b,BTSTAT);
-    (void)memcpy((char *)st,b.lbuf,sizeof *st);
+    (void)memcpy(&st,b.lbuf,sizeof st);
     btas(&b,BTCLOSE);
   enverr
     if (rc == 209)
       b.root = 0L;
-    printf("%s: STAT failed (rc == %d), using defaults\n",name,rc);
-    st->bcnt = 0;	/* can't get status */
-    st->rcnt = 0;
-    st->mtime = st->atime = time(&st->ctime);
-    st->links = 0;
-    st->opens = 0;
-    st->id.user = getuid();
-    st->id.group = getgid();
-    st->id.mode = 0666;
+    if (interactive)
+      printf("%s: STAT failed (rc == %d), using defaults\n",name,rc);
+    st.bcnt = 0;	/* can't get status */
+    st.rcnt = 0;
+    st.mtime = st.atime = time(&st.ctime);
+    st.links = 0;
+    st.opens = 0;
+    st.id.user = getuid();
+    st.id.group = getgid();
+    st.id.mode = 0666;
   envend
   if (b.mid == dirf->mid && b.root == dirf->root)
     return 0L;	/* doesn't exist */
