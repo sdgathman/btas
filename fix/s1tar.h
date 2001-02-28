f65204e5999294f7a240a84961d8ac9cb94a36a7
Stuart Gathman <stuart@gathman.org>
2001-02-28 22:54:27 +0000

    use new logdir API

--- fix/s1tar.h
+++ fix/s1tar.h
@@ -1,13 +1,16 @@
+#include "logdir.h"
 #include "s1fs.h"
 #include <String.h>
 
-class s1tar {
+class s1tar: public Logdir {
   String curdir;
   int fd;
   s1FS *fs;
   void seqout(void *,int);
   void chdir(const char *dir);
   void eof(int rlen);
+protected:
+  void doroot(root_n *r);
 public:
   enum { ECOMMA = 0x6B };
   s1tar(int f,s1FS *);
