df1ca69c7b1b655de86fc16cac8a62939c3784f5
Stuart Gathman <stuart@gathman.org>
2001-02-28 21:55:51 +0000

    file/mount manager object

--- btfile.h
+++ btfile.h
@@ -1,15 +1,54 @@
 /*
-	File and directory handling module.
+	Intermediate level key lookup routines.  See btkey.c for
+	a more detailed explanation.
 */
-	
-int openfile(BTCB *,int);	/* open/stat file, check security */
-void closefile(BTCB *);		/* close file */
-t_block creatfile(BTCB *);	/* create new file */
-t_block linkfile(BTCB *);	/* link existing file */
-int delfile(BTCB *, t_block);	/* unlink file */
-int btjoin(BTCB *);		/* join file */
-int btunjoin(BTCB *);		/* unjoin file */
 
-struct btredirect {
-  long pid;		/* server process */
+#pragma interface
+
+#include "btree.h"
+
+class BLOCK;
+class BlockCache;
+struct BTCB;
+
+class bttrace {
+  btlevel stack[MAXLEV];
+  int getkey(BLOCK *, BLOCK *, char *, t_block *);
+  int insert(BLOCK *, BLOCK *, int, char *, int, btlevel *);
+protected:
+  void btfree(BLOCK *blk);
+  BLOCK *btnew(short flags);
+  BlockCache *bufpool;
+  BLOCK *btbuf(t_block node);
+  btlevel *sp;	// FIXME: provide stack interface
+public:
+  bttrace(BlockCache *);
+  BLOCK *trace(struct BTCB *, int, int);
+  int needed() const { return sp - stack + 2; }	// worst case blocks needed
+  void add(char *urec, int ulen);
+  void del();
+};
+
+struct btfile: private bttrace {
+  btfile(BlockCache *c);
+  BLOCK *uniquekey(BTCB *);	/* find unique key */
+  BLOCK *btfirst(BTCB *);		/* find first matching key */
+  BLOCK *btlast(BTCB *);		/* find last matching key */
+  BLOCK *verify(BTCB *);		/* verify cached location */
+  bttrace::trace;
+  int addrec(BTCB *);		/* add user record */
+  void delrec(BTCB *, BLOCK *);	/* delete user record */
+  void replace(BTCB *b,bool rew = false);
+  //	File and directory handling
+  int openfile(BTCB *,int);	/* open/stat file, check security */
+  void closefile(BTCB *);		/* close file */
+  t_block creatfile(BTCB *);	/* create new file */
+  t_block linkfile(BTCB *);	/* link existing file */
+  int delfile(BTCB *, t_block);	/* unlink file */
+  int join(BTCB *);		/* join file */
+  int unjoin(BTCB *);		/* unjoin file */
+private:
+  int joincnt;
+  struct joinrec;
+  joinrec *jointbl;
 };
