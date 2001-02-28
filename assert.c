e26a0ad83f3c5b97a8eb4d6a3af86a26199e13f0
Stuart Gathman <stuart@gathman.org>
2001-02-28 21:24:47 +0000

    support linux assert

--- assert.c
+++ assert.c
@@ -3,7 +3,21 @@
 #include <bterr.h>
 #include "btree.h"
 
+#ifndef ASSERTFCN
+#define ASSERTFCN _assert
+#endif
+extern "C"
 volatile void ASSERTFCN(const char *ex,const char *file,int line) {
   fprintf(stderr,"Assertion failure: %s line %d \"%s\"\n",file,line,ex);
   btpost(BTERINT);
 }
+#if defined(linux)
+extern "C" void __assert_fail (__const char *ex,
+                                __const char *file,
+                                unsigned int line,
+                                __const char *function)
+      {
+  fprintf(stderr,"Assertion failure: %s %s:%d \"%s\"\n",file,function,line,ex);
+  btpost(BTERINT);
+}
+#endif
