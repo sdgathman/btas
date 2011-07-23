7eca9a78de135ba2a537c28e89deabab8b125965
Stuart Gathman <stuart@gathman.org>
2011-07-23 19:44:18 +0000

    Fix cache handling

--- btstart.sh
+++ btstart.sh
@@ -10,6 +10,7 @@ test -s "${FSTAB}" || {
   echo "Missing ${FSTAB}"
   exit 1
 }
+umask 002
 exec >>"/var/log/btas/btserve$BTSERVE.log" 2>&1 </dev/null
 
 blksiz=1024
@@ -17,20 +18,24 @@ cache=2000000
 rootfs=
 while read fs mnt tb back; do
   case "$fs" in
-  "#") continue;;
+  "#"*) continue;;
+  esac
+  case "$mnt" in
+  "cache")	cache="$tb"; continue;;
+  "/")		rootfs="$fs";;
   esac
   [ "$tb" -gt "$blksiz" ] && blksiz="$tb"
-  [ "$mnt" = "/" ] && rootfs="$fs"
-  [ "$mnt" = "cache" ] && cache="$tb"
 done <"${FSTAB}"
 
 /bms/bin/btserve -b "${blksiz}" -c "${cache}" "${rootfs}"
 
 while read fs mnt tb back; do
   case "$fs" in
-  "#") continue;;
+  "#"*) continue;;
   esac
-  [ "$mnt" = "/" ] && continue
-  [ "$mnt" = "cache" ] && continue
+  case "$mnt" in
+  "cache")	continue;;
+  "/")		continue;;
+  esac
   /bms/bin/btutil "mo $fs $mnt"
 done <"${FSTAB}"
